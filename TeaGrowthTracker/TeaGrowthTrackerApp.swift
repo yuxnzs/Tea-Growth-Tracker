90ec21fa85f46f990389238d8c9b46cd199b0117
yuxnzs <yuchengliu0429@gmail.com>
2024-06-15 23:03:37 +0800

    Init project

---
+++ TeaGrowthTracker/TeaGrowthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeaGrowthTrackerApp.swift
+//  TeaGrowthTracker
+//
+//  Created by yucheng on 2024/6/15.
+//
+
+import SwiftUI
+
+@main
+struct TeaGrowthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
