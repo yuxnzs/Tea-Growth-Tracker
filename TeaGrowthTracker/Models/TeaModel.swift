3d26f8a4055b98a23ba574c659757a755fb66cac
yuxnzs <yuchengliu0429@gmail.com>
2024-06-18 17:33:42 +0800

    Rename TeaData to TeaModel

---
+++ TeaGrowthTracker/Models/TeaModel.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+class TeaModel: Identifiable, ObservableObject {
+    let id = UUID()
+    let teaGardenID: Int
+    let name: String
+    let location: String
+    let originalImage: String
+    let analyzedImage: String
+    let date: String
+    let weather: String
+    let growth: String
+    let waterFlow: String
+    
+    init(teaGardenID: Int, name: String, location: String, originalImage: String, analyzedImage: String, date: String, weather: String, growth: String, waterFlow: String) {
+        self.teaGardenID = teaGardenID
+        self.name = name
+        self.location = location
+        self.originalImage = originalImage
+        self.analyzedImage = analyzedImage
+        self.date = date
+        self.weather = weather
+        self.growth = growth
+        self.waterFlow = waterFlow
+    }
+}
