142dcd4e56784537c6bd57f5599766b38e6c2260
yuxnzs <yuchengliu0429@gmail.com>
2024-10-04 00:55:55 +0800

    Update FullImageView to handle UserTeaImage's UIImage

--- TeaGrowthTracker/Views/FullImageView.swift
+++ TeaGrowthTracker/Views/FullImageView.swift
@@ -3,39 +3,50 @@ import SwiftUI
 struct FullImageView: View {
     @Binding var selectedTab: Int // 預設顯示選項
     let isAnalysisView: Bool
-    
-    let images: [String]
+    let asyncImages: [String]?
+    let useUIImage: Bool // UserTeaImage 使用 UIImage
+    let uiImage: UIImage?
     
     var body: some View {
         if isAnalysisView {
-            TabView(selection: $selectedTab) {
-                ForEach(images.indices, id: \.self) { index in
-                    AsyncImage(url: URL(string: images[index])) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                    } placeholder: {
-                        ProgressView()
-                            .tint(.white)
+            // AnalysisView 使用 selectedTab 來追蹤目前圖片選中的 index
+            if let asyncImages = asyncImages {
+                TabView(selection: $selectedTab) {
+                    ForEach(asyncImages.indices, id: \.self) { index in
+                        AsyncImage(url: URL(string: asyncImages[index])) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            ProgressView()
+                                .tint(.white)
+                        }
+                        .tag(index) // 設置標籤選項
                     }
-                    .tag(index) // 設置標籤選項
                 }
+                .tabViewStyle(PageTabViewStyle())
+                .background(.black)
             }
-            .tabViewStyle(PageTabViewStyle())
-            .background(.black)
         } else {
             // 不用 selectedTab 時，以預設方式顯示
             TabView {
-                ForEach(images.indices, id: \.self) { index in
-                    AsyncImage(url: URL(string: images[index])) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                    } placeholder: {
-                        ProgressView()
-                            .tint(.white)
+                if useUIImage, let uiImage = uiImage {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                        .tag(0)
+                } else if let asyncImages = asyncImages {
+                    ForEach(asyncImages.indices, id: \.self) { index in
+                        AsyncImage(url: URL(string: asyncImages[index])) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            `ProgressView`()
+                                .tint(.white)
+                        }
+                        .tag(index) // 設置標籤選項
                     }
-                    .tag(index) // 設置標籤選項
                 }
             }
             .tabViewStyle(PageTabViewStyle())
@@ -48,8 +59,11 @@ struct FullImageView: View {
     FullImageView(
         selectedTab: .constant(0),
         isAnalysisView: true,
-        images: [
+        asyncImages: [
             "https://plus.unsplash.com/premium_photo-1692049124070-87d5ddfea09a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "https://images.unsplash.com/photo-1605105777592-c3430a67d033?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        ])
+        ],
+        useUIImage: false,
+        uiImage: nil
+    )
 }
