44ce3b2e9ad59b69c9e497e11b97ba4d80efb2cc
yuxnzs <yuchengliu0429@gmail.com>
2024-10-11 16:40:39 +0800

    Update ActionLoadingView background opacity to 0.2 and ProgressView tint to white

--- TeaGrowthTracker/Views/ActionLoadingView.swift
+++ TeaGrowthTracker/Views/ActionLoadingView.swift
@@ -3,9 +3,10 @@ import SwiftUI
 struct ActionLoadingView: View {
     var body: some View {
         ZStack {
-            Color.black.opacity(0.5)
+            Color.black.opacity(0.2) // 與系統的 alert 出現時背景透明度相似
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
             ProgressView()
+                .tint(.white)
         }
         .ignoresSafeArea()
     }
