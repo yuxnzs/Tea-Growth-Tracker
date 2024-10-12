701726240b2a86e2261972e564e6477c36ac4ac8
yuxnzs <yuchengliu0429@gmail.com>
2024-10-12 21:20:01 +0800

    Remove white tint from ProgressView in ActionLoadingView for better visibility

--- TeaGrowthTracker/Views/ActionLoadingView.swift
+++ TeaGrowthTracker/Views/ActionLoadingView.swift
@@ -6,7 +6,6 @@ struct ActionLoadingView: View {
             Color.black.opacity(0.2) // 與系統的 alert 出現時背景透明度相似
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
             ProgressView()
-                .tint(.white)
         }
         .ignoresSafeArea()
     }
