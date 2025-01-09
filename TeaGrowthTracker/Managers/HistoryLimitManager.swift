79af4a0204893b20c0e8b0e44382aad63370740e
yuxnzs <yuchengliu0429@gmail.com>
2025-01-09 23:15:13 +0800

    Update historyLimit to 20 in HistoryLimitManager

--- TeaGrowthTracker/Managers/HistoryLimitManager.swift
+++ TeaGrowthTracker/Managers/HistoryLimitManager.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 class HistoryLimitManager: ObservableObject {
-    @Published var historyLimit: Int = 5
+    @Published var historyLimit: Int = 20
     
     func hasReachedLimit(diseaseCount: Int) -> Bool {
         return diseaseCount >= historyLimit
