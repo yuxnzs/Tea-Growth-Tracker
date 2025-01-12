7d3af1e6fc719df4751ea6afdfbef0a4ee88973b
yuxnzs <yuchengliu0429@gmail.com>
2025-01-12 23:59:33 +0800

    Update HistoryLimitManager: Add currentHistoryCount with UserDefaults; implement increment and decrement methods; replace diseaseCount with currentHistoryCount in hasReachedLimit

--- TeaGrowthTracker/Managers/HistoryLimitManager.swift
+++ TeaGrowthTracker/Managers/HistoryLimitManager.swift
@@ -2,8 +2,34 @@ import SwiftUI
 
 class HistoryLimitManager: ObservableObject {
     @Published var historyLimit: Int = 20
-    
-    func hasReachedLimit(diseaseCount: Int) -> Bool {
-        return diseaseCount >= historyLimit
+    @Published var currentHistoryCount: Int {
+        // 當數值更新時，儲存到 UserDefaults
+        didSet {
+            UserDefaults.standard.set(currentHistoryCount, forKey: "currentHistoryCount")
+        }
     }
+    
+    init() {
+        // 從 UserDefaults 初始化 currentHistoryCount
+        self.currentHistoryCount = UserDefaults.standard.integer(forKey: "currentHistoryCount")
+    }
+    
+    func hasReachedLimit() -> Bool {
+        return currentHistoryCount >= historyLimit
+    }
+    
+    // 增加 currentHistoryCount
+    func incrementCount() {
+        if currentHistoryCount < historyLimit {
+            currentHistoryCount += 1
+        }
+    }
+    
+    // 減少 currentHistoryCount
+    func decrementCount() {
+        if currentHistoryCount > 0 {
+            currentHistoryCount -= 1
+        }
+    }
+    
 }
